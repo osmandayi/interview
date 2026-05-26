b6e180cb9e1cbe6cb8975ec96cc300ae4ede8b65
Osman <88623469+osmandayi@users.noreply.github.com>
2026-05-26 17:36:40 +0300

    feat(types): add QA data types

---
+++ src/types/qa.ts
@@ -0,0 +1,29 @@
+export type QAData = {
+  categories: Category[];
+};
+
+export type Category = {
+  id: string;
+  title: string;
+  subcategories: Subcategory[];
+};
+
+export type Subcategory = {
+  id: string;
+  title: string;
+  items: QAItem[];
+};
+
+export type QAItem = {
+  id: string;
+  question: string;
+  answer: string;
+  tags?: string[];
+};
+
+export type FlatQAItem = QAItem & {
+  categoryId: string;
+  categoryTitle: string;
+  subcategoryId: string;
+  subcategoryTitle: string;
+};
